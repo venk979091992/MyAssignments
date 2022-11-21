76d0612de79d073abecb888b9c02d4209397c00c
venk979091992 <117563887+venk979091992@users.noreply.github.com>
2022-11-22 00:56:41 +0530

    Update DeleteLead.java

--- week2day2Assignment/DeleteLead.java
+++ week2day2Assignment/DeleteLead.java
@@ -27,33 +27,31 @@ public class DeleteLead {
 		driver.findElement(By.className("decorativeSubmit")).click();
 		
 		driver.findElement(By.linkText("CRM/SFA")).click();
-		//driver.findElement(By.linkText("Leads")).click();
 		driver.findElement(By.xpath("//a[@href='/crmsfa/control/leadsMain']")).click();
 		driver.findElement(By.xpath("//a[@href='/crmsfa/control/findLeads']")).click();
 		driver.findElement(By.xpath("//span[text()='Phone']")).click();
-		driver.findElement(By.xpath("//input[@name='phoneAreaCode']")).sendKeys("123");
-		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("98431009");
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("99");
 					
 		driver.findElement(By.xpath("//button[@id='ext-gen334']")).click();
 		
 		Thread.sleep(4000);
 		String Leadid = driver.findElement(By.xpath("//div[@class = 'x-grid3-cell-inner x-grid3-col-partyId']/a[@class='linktext']")).getText();
 		
-		 System.out.println("id is "+ Leadid);
+		System.out.println("id is "+ Leadid);
 		driver.findElement(By.xpath("//div[@class = 'x-grid3-cell-inner x-grid3-col-partyId']/a[@class='linktext']")).click();
 		
 			       
 	    driver.findElement(By.xpath("//a[@class='subMenuButtonDangerous']")).click();
-	    
+	   
 	  
-	    driver.findElement(By.xpath("//a[text()='Find Leads']")).click();
-	    //System.out.println("id is "+ Leadid);
+	    driver.findElement(By.xpath("//a[text()='Find Leads']")).click(); //modified
+	    driver.findElement(By.xpath("//input[@name='id']")).sendKeys(Leadid);//modified
+		driver.findElement(By.xpath("//button[@id='ext-gen334']")).click();//modified
+	    
+	    Thread.sleep(2000);
+	    String norecord = driver.findElement(By.xpath("//div[@class='x-paging-info']")).getText();//modified
+	    System.out.println(norecord);//modified
 		
-	    //driver.findElement(By.xpath("//span [= )).click();
-	    
-	    String norecord = driver.findElement(By.xpath("//a[@class='x-paging-info']")).getText();
-	    
-		//if()
 		
 	}
 
